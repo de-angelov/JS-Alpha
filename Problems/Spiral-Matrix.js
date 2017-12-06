c956f5f491950e75ea169ce05e3e4963261c6e06
Denis Angelov <de.angelov.bg@gmail.com>
2017-12-06 13:27:22 +0200

    Fixed Spiral-Matrix.js Problem

--- Problems/Spiral-Matrix.js
+++ Problems/Spiral-Matrix.js
@@ -5,50 +5,46 @@ const getGets = (arr) => {
     };
 };
 
-const gets = this.gets || getGets(['3']);
+const gets = this.gets || getGets(['6']);
 const print = this.print || console.log;
 
 
 const buildMatrix = (n) => {
-    const matrix= Array(n).fill().map(()=>Array(n).fill());
+    const matrix = Array(n).fill().map(() => Array(n).fill());
 
     const deltaX = [1, 0, -1, 0];
     const deltaY = [0, 1, 0, -1];
     let action = 0;
     let x = 0;
     let y = 0;
-    const isNotInMatrix =(z, j)=>{
-        if (z < 0 || z > j-1) {
+
+
+    const isNotInMatrix = (z, j) => {
+        if (z < 0 || z > j - 1) {
             return true;
         }
-
-            return false;
+        return false;
     };
-    // const nextStepIsFull =(i, j)=>{
-    //     if (matrix[i][j] === 'undefined' ) {
-    //         return false;
-    //     }
-    //     return true;
-    // };
+    const nextStepIsFull = (in1, in2) => {
+        if (Number.isInteger(matrix[in1][in2])) {
+            return true;
+        }
+        return false;
+    };
 
-    for (let i = 1; i < (n * n)+1; i += 1) {
+    for (let i = 1; i < (n * n) + 1; i += 1) {
         matrix[y][x] = i;
 
 
-        if (isNotInMatrix(x +deltaX[action], n)) {
+        if (isNotInMatrix(x + deltaX[action], n)||
+           (isNotInMatrix(y + deltaY[action], n))||
+           (nextStepIsFull(y + deltaY[action], x + deltaX[action]))) {
             action += 1;
             action = action % 4;
         }
-        if (isNotInMatrix(y +deltaY[action],x +deltaX[action])) {
-            action += 1;
-            action = action % 4;
-        }
-        // if (nextStepIsFull(x,y,matrix)) {
-        //     action += 1;
-        //     action = action % 4;
-        // }
-        x +=deltaX[action];
-        y +=deltaY[action];
+
+        x += deltaX[action];
+        y += deltaY[action];
     }
     return matrix;
 };
@@ -56,4 +52,15 @@ const buildMatrix = (n) => {
 
 const N = Number(gets());
 const result = buildMatrix(N);
-print(result);
+let output = '';
+
+for (let i=0; i<result.length; i+=1) {
+    output=result[i][0];
+    for (let j=1; j<result[i].length; j++) {
+    output+=` ${result[i][j]}`;
+    }
+    print(output);
+}
+
+Quit(0);
+
